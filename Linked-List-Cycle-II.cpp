f272354418e1c5bab49059f949c64642de4c44d9
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-04-01 15:20:32 +0800

    Create Linked-List-Cycle-II.cpp

---
+++ Linked-List-Cycle-II.cpp
@@ -0,0 +1,37 @@
+/*
+*Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+*Follow up:
+*Can you solve it without using extra space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow)
+               break;
+        }
+        if(fast != NULL && fast->next != NULL){
+            ListNode *detect = head;
+            while(fast != detect){
+                detect = detect->next;
+                fast = fast->next;
+            }
+                return detect;
+        }
+    return NULL;
+    }
+};
