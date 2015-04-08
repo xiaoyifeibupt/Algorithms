2f4b9022f928c2d0acee24c411cf3d7ecd3d2eb9
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-04-08 20:37:45 +0800

    Update Minimum-Depth-of-Binary-Tree.cpp

--- leetcode/Minimum-Depth-of-Binary-Tree.cpp
+++ leetcode/Minimum-Depth-of-Binary-Tree.cpp
@@ -18,8 +18,7 @@ public:
     int minDepth(TreeNode *root) {
         if(root == NULL)
             return 0;
-        if(root -> left == NULL && root -> right == NULL)
-            return 1;
+            
         int leftDepth = minDepth(root->left);
         int rightDepth = minDepth(root->right);
         
