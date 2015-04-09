4477afa813b5864c876863e621815beb9bad8ed7
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-04-09 20:36:00 +0800

    Create Plus-One.cpp

---
+++ leetcode/Plus-One.cpp
@@ -0,0 +1,25 @@
+/**
+*Given a non-negative number represented as an array of digits, plus one to the number.
+
+*The digits are stored such that the most significant digit is at the head of the list.
+*/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int flag = 1;
+        for(auto d = digits.rbegin(); d != digits.rend(); ++d){
+            *d += flag;
+            if (*d >= 10) {
+                *d %= 10;
+                flag = 1; 
+            }
+            else
+                flag = 0;
+        }
+        if (flag)
+            digits.insert(digits.begin(), flag);
+        
+        return digits;
+    }
+};
