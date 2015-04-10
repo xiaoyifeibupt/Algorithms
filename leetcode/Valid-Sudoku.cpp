52d93284b2bffd9e5b3079c985eb816454e8ba46
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-04-10 15:27:31 +0800

    Create Valid-Sudoku.cpp

---
+++ leetcode/Valid-Sudoku.cpp
@@ -0,0 +1,26 @@
+/**
+Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+*/
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char> > &board) {
+        vector<vector<bool>> rows(9, vector<bool>(9,false));  
+        vector<vector<bool>> cols(9, vector<bool>(9,false));  
+        vector<vector<bool>> blocks(9, vector<bool>(9,false));  
+  
+        for(int i = 0; i < 9; i++)  
+            for(int j = 0; j < 9; j++)  
+            {  
+                if(board[i][j] == '.')continue;  
+                int num = board[i][j] - '1';  
+                if(rows[i][num] || cols[j][num] || blocks[i - i%3 + j/3][num])  
+                    return false;  
+                rows[i][num] = cols[j][num] = blocks[i - i%3 + j/3][num] = true;  
+            }  
+        return true;  
+    }
+};
