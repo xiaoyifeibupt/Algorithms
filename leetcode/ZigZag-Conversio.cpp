aa233d20801f7a94e16fec2ffb8f90fc6da1cc48
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-04-11 16:57:22 +0800

    Update ZigZag-Conversio.cpp

--- leetcode/ZigZag-Conversio.cpp
+++ leetcode/ZigZag-Conversio.cpp
@@ -11,7 +11,7 @@
 *convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 */
 
-class Solution {
+class Solution1 {
 public:
     string convert(string s, int nRows) {
         if(nRows == 1) return s;
@@ -25,5 +25,45 @@ public:
         for(i = 0; i < nRows; ++i)
             str += res[i];
     return str;
+    }
+};
+
+/**
+ * 0        8         16
+ * 1      7 9       1517
+ * 2    6   10    14  18
+ * 3  5     11  13    19
+ * 4        12        20    
+ * 第0行和最后一行中，前一个下标的值和后一个下标的值相差 2 * nRows - 2
+ * 中间行中，前一个下标的值和后一个下标的值需要根据这个下标是该行中的奇数列还是偶数列来计算
+ * 奇数列2 * (nRows - 1 - i)，偶数列2 * i。
+ * /
+class Solution2 {
+public:
+    string convert(string s, int nRows) {
+        if (nRows <= 1 || s.length() == 0)  
+           return s;  
+  
+        string res = "";  
+        int len = s.length();  
+        for (int i = 0; i < len && i < nRows; ++i) {  
+            int indx = i;  
+            res += s[indx];  
+  
+            for (int k = 1; indx < len; ++k) {
+                if (i == 0 || i == nRows - 1)
+                    indx += 2 * nRows - 2;
+                else {  
+                    if (k & 0x1)
+                        indx += 2 * (nRows - 1 - i);  
+                    else 
+                        indx += 2 * i;  
+                }
+                if (indx < len) {  
+                    res += s[indx];  
+                }     
+            }  
+        }  
+        return res;  
     }
 };
