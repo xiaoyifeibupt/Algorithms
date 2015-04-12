bb718df951f99c6231db08cbc9ff5cdda349dfc3
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-04-12 16:24:05 +0800

    Create Roman-to-Integer.cpp

---
+++ leetcode/Roman-to-Integer.cpp
@@ -0,0 +1,33 @@
+/**
+*Given a roman numeral, convert it to an integer.
+
+*Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result=0;
+        
+        map<char,int> roman;
+        roman['I']=1;
+        roman['V']=5;
+        roman['X']=10;
+        roman['L']=50;
+        roman['C']=100;
+        roman['D']=500;
+        roman['M']=1000;
+        
+        for(int i=s.length()-1;i>=0;i--) {
+            if(i==s.length()-1) {
+                result=roman[s[i]];
+                continue;
+            }
+            if(roman[s[i]] >= roman[s[i+1]])
+                result+=roman[s[i]];
+            else
+                result-=roman[s[i]];
+        }
+        return result;
+    }
+};
