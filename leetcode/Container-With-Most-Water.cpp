75fc1435f9d1bc3b714909368e27d54418733755
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-06-02 21:10:04 +0800

    Create Container-With-Most-Water.cpp

---
+++ leetcode/Container-With-Most-Water.cpp
@@ -0,0 +1,21 @@
+/*
+8Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+8Note: You may not slant the container.
+*/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0,j = height.size() - 1;
+        int cap = 0;
+        while(i < j) {
+            cap = max(cap,(j - i) * min(height[i],height[j]));
+            if(height[i] < height[j])
+                i++;
+            else
+                j--;
+        }
+        return cap;
+    }
+};
