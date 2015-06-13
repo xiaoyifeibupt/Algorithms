f719eb2acd9b40f8968b4246498a6e2cc1f0da6a
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-06-13 11:04:27 +0800

    Create Invert-Binary-Tree.cpp

---
+++ leetcode/Invert-Binary-Tree.cpp
@@ -0,0 +1,41 @@
+/*
+*Invert a binary tree.
+
+       4
+     /   \
+    2     7
+   / \   / \
+  1   3 6   9
+  to
+       4
+     /   \
+    7     2
+   / \   / \
+  9   6 3   1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) 
+            return NULL;
+        
+        TreeNode* leftNode = root -> left;
+        TreeNode* rightNode = root -> right;
+        root -> left = invertTree(rightNode);
+        root -> right = invertTree(leftNode);
+        
+        return root;
+            
+    }
+};
+
