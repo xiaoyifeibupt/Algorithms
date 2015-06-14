7ee0a9ccbdad8d03075ab6da65f0edbaa9a0f979
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-06-14 10:58:46 +0800

    Create Pow(x, n).cpp

---
+++ leetcode/Pow(x, n).cpp
@@ -0,0 +1,23 @@
+/*
+*Implement pow(x, n).
+*/
+
+class Solution {
+public:
+    double powhelp(double x, int n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        double result = myPow(x,n>>1);
+        result *= result;
+        
+        if(n & 0x1 == 1)
+            result *= x;
+        return result;
+    }
+    double myPow(double x, int n) {
+        if(n < 0)
+            return 1.0 / powhelp(x, -n);
+        else
+            return powhelp(x, n);
+    }
+};
