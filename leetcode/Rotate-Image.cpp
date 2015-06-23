03c8a987062e1d5723f5585945c4dd1c8b8cd697
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-06-23 21:13:52 +0800

    Create Rotate-Image.cpp

---
+++ leetcode/Rotate-Image.cpp
@@ -0,0 +1,25 @@
+/*
+*You are given an n x n 2D matrix representing an image.
+
+*Rotate the image by 90 degrees (clockwise).
+
+*Follow up:
+*Could you do this in-place?
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int limit = (n-1)/2;
+        for(int i = 0; i <= limit; i++){
+            for(int j = i; j < n-1-i; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            }
+        }
+    }
+};
