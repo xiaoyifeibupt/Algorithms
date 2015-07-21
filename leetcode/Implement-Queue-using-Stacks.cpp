aa0a5169afe7ea167ab2a9a186503c7d26b5f4be
yifei XIAO <xiaoyifeibupt@outlook.com>
2015-07-21 17:22:44 +0800

    Create Implement-Queue-using-Stacks.cpp

---
+++ leetcode/Implement-Queue-using-Stacks.cpp
@@ -0,0 +1,57 @@
+/**
+*Implement the following operations of a queue using stacks.
+
+*push(x) -- Push element x to the back of queue.
+*pop() -- Removes the element from in front of queue.
+*peek() -- Get the front element.
+*empty() -- Return whether the queue is empty.
+*Notes:
+*You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+*Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+*You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+*/
+
+class Queue {
+public:
+    // Push element x to the back of queue.
+    void push(int x) {
+        in.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    void pop(void) {
+        if(out.empty()) {
+            while(!in.empty()) {
+                int x = in.top();
+                in.pop();
+                out.push(x);
+            }
+        }
+        if(!out.empty()) {
+            out.pop();
+        }
+    }
+
+    // Get the front element.
+    int peek(void) {
+        if(out.empty()) {
+            while(!in.empty()) {
+                int x = in.top();
+                in.pop();
+                out.push(x);
+            }
+        }
+        if(!out.empty()) {
+            return out.top();
+        }
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return in.empty() && out.empty();
+    }
+    
+private:
+    stack<int> in;
+    stack<int> out;
+};
